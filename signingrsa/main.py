f7d75f57b0d96668f603a7e2b9dd56752e1212dd
Plínio Mayer <pctmayer@gmail.com>
2021-05-08 16:52:20 -0300

    add first arg (-k).

--- signingrsa/main.py
+++ signingrsa/main.py
@@ -1,4 +1,21 @@
+import argparse
+
+def genereate_keys():
+    print()
+
+def config_argparse():
+    parser = argparse.ArgumentParser(description='Generates key and signs messages using RSA.')
+    parser.add_argument('-k', action = 'store_true', help = 'Generates private key (private.key) and public key (public.key) in keys folder')
+    return parser
+
+
 def main():
+    parser = config_argparse()
+    arguments = parser.parse_args()
+
+    if (arguments.k):
+        generate_keys()
+
     print('Done! Press Enter to close.')
     input()
 
