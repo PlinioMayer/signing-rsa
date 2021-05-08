4c01b165bed1b326949370078cc43715ca9ee414
Plínio Mayer <pctmayer@gmail.com>
2021-05-08 18:28:57 -0300

    generating keys.

--- signingrsa/main.py
+++ signingrsa/main.py
@@ -1,16 +1,7 @@
 import argparse
 from random import randrange, getrandbits
-
-def generate_keys():
-    print('Generating keys:')
-    p = generate_prime_number()
-    q = generate_prime_number()
-
-    n = p * q
-    e = 65537
-    
-
-
+import pathlib
+import os
 
 def config_argparse():
     parser = argparse.ArgumentParser(description='Generates key and signs messages using RSA.')
@@ -27,10 +18,31 @@ def main():
     print('Done! Press Enter to close.')
     input()
 
+def generate_keys():
+    p = generate_prime_number()
+    q = generate_prime_number()
+    n = p * q
+        
+    print('Generating e that is relatively prime to (p-1)*(q-1)...')
+    while True:
+        e = randrange(2 ** 0, 2 ** 16)
+        if gcd(e, (p - 1) * (q - 1)) == 1:
+            break
+    
+    print('Calculating d that is mod inverse of e...')
+    d = findModInverse(e, (p - 1) * (q - 1))
 
-if __name__ == '__main__':
-    main()
+    folder_path = pathlib.Path('keys')
+   
+    if (not os.path.exists(folder_path)):
+        os.makedirs(folder_path)
 
+    print(f"Saving keys in {folder_path}...")
+    with open(pathlib.Path("keys/public.key").absolute(), 'w', encoding = 'utf-8') as file:
+        file.write(f"n={n}\ne={e}")
+
+    with open(pathlib.Path(f"keys/private.key").absolute(), 'w', encoding = 'utf-8') as file:
+        file.write(f"n={n}\nd={d}")
 
 def is_prime(n, k=128):
 
@@ -68,9 +80,27 @@ def generate_prime_candidate(length):
     return p
 
 def generate_prime_number(length=1024):
-    
     p = 4
     # keep generating while the primality test fail
     while not is_prime(p, 128):
         p = generate_prime_candidate(length)
     return p
+
+def gcd(a, b):
+    while a != 0:
+        a, b = b % a, a
+    return b
+
+def findModInverse(a, m):
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    
+    while v3 != 0:
+        q = u3 // v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+if __name__ == '__main__':
+   main()
